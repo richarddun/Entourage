4b38c0d9c46e66594738f559f1ced08ba180e9f4
Richard Dunne <richard.w.dunne@gmail.com>
2023-05-28 20:05:18 +0100

    some cleanup, adding scrollview for textinput

--- base.py
+++ base.py
@@ -1,9 +1,16 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.core.window import Window
 
 class ChattorApp(App):
     def build(self):
         return ChattorFlow()
+    
+    def submit(self):
+        self.print_widget_properties()
+
+    def print_widget_properties(self):
+        self.root.ids.outputwidget.text += self.root.ids.inputwidget.text
 
 class ChattorFlow(Widget):
     pass
