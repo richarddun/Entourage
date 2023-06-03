5bb29e741c75a9aa01efeff3aebdc2713d2750b7
Richard Dunne <richard.w.dunne@gmail.com>
2023-06-03 13:56:16 +0100

    Adding historical messaging, no limit for now

--- base.py
+++ base.py
@@ -3,8 +3,6 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
 import openai
 import os
-#import asyncio
-#from kivy.base import EventLoop
 from kivy.clock import Clock
 
 class AICommunicator():
@@ -12,27 +10,33 @@ class AICommunicator():
     def __init__(self) -> None:
         openai.api_key = os.getenv("OPENAI_API_KEY")
         self.api_key = os.getenv("OPENAI_API_KEY")
+        self.prompt_history = [{"role":"system","content":"You are a helpful assistant"}]
+
+    def get_prompt_history(self):
+        return self.prompt_history
+    
+    def clear_prompt_history(self):
+        self.prompt_history = [{"role":"system","content":"You are a helpful assistant"}]
 
     def evaluate(self,prompt):
-        # send transcript to openAI for chat completion which will summarise the text
+        self.prompt_history.append({"role":"user","content":f"{prompt}"})
         response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages = [
-        {"role":"system","content":"You are a helpful assistant"},
-        {"role":"user","content":f"{prompt}"
-    }
-        ]
+        messages = self.prompt_history
     )
+        self.prompt_history.append({"role":"assistant","content":f"{response.choices[0].message.content.strip()}"})
+        print(self.prompt_history)
         return response.choices[0].message.content.strip()
 
 class ChattorApp(App):
 
     def build(self):
         self.cleared = False
+        self.oai = AICommunicator()
         return ChattorFlow()
     
     def submit(self):
-        self.oai = AICommunicator()
+        #self.oai = AICommunicator()
         response = self.oai.evaluate(self.root.ids.inputwidget.text)
         #self.root.ids.outputwidget.text = '...Loading response'
         self.root.ids.outputwidget.text = response
